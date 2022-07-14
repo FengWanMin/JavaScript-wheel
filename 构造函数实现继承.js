878a55d7e5b9251228904cb8419e7d4dff330289
冯万敏 <39734870+FengWanMin@users.noreply.github.com>
2022-07-14 16:38:27 +0800

    Update 构造函数实现继承.js

--- 构造函数实现继承.js
+++ 构造函数实现继承.js
@@ -1,3 +1,4 @@
+// 借用构造函数实现继承解决了原型链继承的 2 个问题：引用类型共享问题以及传参问题。但是由于方法必须定义在构造函数中，所以会导致每次创建子类实例都会创建一遍方法。
 function Animal(name) {
   this.name = name
 }
@@ -8,7 +9,7 @@ function Dog(name, age) {
   Animal.call(this, name)
   this.age = age
 }
-Dog.prototype = Animal.prototype
+// Dog.prototype = Animal.prototype
 const _dog = new Dog('luck', 2)
 _dog.name // luck
 _dog.age // 2
